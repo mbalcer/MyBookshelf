f823f24e3e5a007a21427889ae1fb999ef6caf09
mbalcer <mateusz75319@gmail.com>
2021-05-31 17:46:01 +0200

    Added fragments to menu item

--- app/src/main/java/pl/edu/utp/mybookshelf/MainActivity.java
+++ app/src/main/java/pl/edu/utp/mybookshelf/MainActivity.java
@@ -1,8 +1,17 @@
 package pl.edu.utp.mybookshelf;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import pl.edu.utp.mybookshelf.fragment.BookshelfFragment;
+import pl.edu.utp.mybookshelf.fragment.ExploreFragment;
+import pl.edu.utp.mybookshelf.fragment.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new BookshelfFragment());
+
+        BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.bookshelf_page:
+                    loadFragment(new BookshelfFragment());
+                    break;
+                case R.id.explore_page:
+                    loadFragment(new ExploreFragment());
+                    break;
+                case R.id.settings_page:
+                    loadFragment(new SettingsFragment());
+                    break;
+                default:
+                    Log.e(MainActivity.class.getName(), "Item id was not found");
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
