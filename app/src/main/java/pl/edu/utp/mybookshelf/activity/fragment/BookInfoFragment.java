d1c1c0f228dd0f56b5d38271be4d7b5fcffc7ede
mbalcer <mateusz75319@gmail.com>
2021-06-05 00:12:04 +0200

    Created fragments to tab layout

---
+++ app/src/main/java/pl/edu/utp/mybookshelf/activity/fragment/BookInfoFragment.java
@@ -0,0 +1,24 @@
+package pl.edu.utp.mybookshelf.activity.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import pl.edu.utp.mybookshelf.R;
+
+public class BookInfoFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_book_info, container, false);
+    }
+}
