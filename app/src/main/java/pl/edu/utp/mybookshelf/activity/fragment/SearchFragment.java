1f82e51ac3668d94bd4c13dc8c0158a33df0884e
mbalcer <mateusz75319@gmail.com>
2021-06-04 15:26:06 +0200

    Created list to displaying found books after searching

--- app/src/main/java/pl/edu/utp/mybookshelf/activity/fragment/SearchFragment.java
+++ app/src/main/java/pl/edu/utp/mybookshelf/activity/fragment/SearchFragment.java
@@ -1,24 +1,73 @@
 package pl.edu.utp.mybookshelf.activity.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import pl.edu.utp.mybookshelf.R;
+import pl.edu.utp.mybookshelf.activity.BookActivity;
+import pl.edu.utp.mybookshelf.adapter.SearchResultListAdapter;
+import pl.edu.utp.mybookshelf.model.Book;
+import pl.edu.utp.mybookshelf.model.Category;
 
 public class SearchFragment extends Fragment {
+
+    private List<Book> booksFound = new ArrayList<>();
+    private ListView searchBookResultList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Book hp = new Book(1l, "J.K. Rowling", "Harry Potter i Kamień Filozoficzny",
+                "Książka „Harry Potter i Kamień Filozoficzny” rozpoczyna cykl o młodym czarodzieju i jego licznych przygodach. " +
+                        "Tytułowy Harry Potter wychowywany jest przez nieprzychylnych mu ciotkę i wuja. Jego rodzice zginęli w tajemniczych " +
+                        "okolicznościach, a jedyne, co mu po nich pozostało to blizna na czole w kształcie błyskawicy. W dniu swoich " +
+                        "11. urodzin bohater dowiaduje się, że istnieje świat, o którym nie miał pojęcia", R.drawable.book_1,
+                "9877323132", 326, LocalDate.of(1997, 10, 1), new Category(1l, "Fantasy"), null);
+
+        booksFound.add(hp);
+        booksFound.add(new Book("J.K. Rowling", "Harry Potter i Komnata Tajemnic", R.drawable.book_1));
+        booksFound.add(new Book("J.K. Rowling", "Harry Potter i Więzień Azkabanu", R.drawable.book_1));
+        booksFound.add(new Book("J.K. Rowling", "Harry Potter i Czara Ognia", R.drawable.book_1));
+        booksFound.add(new Book("Adam Mickiewicz", "Pan Tadeusz", R.drawable.book_2));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_search, container, false);
+
+        searchBookResultList = inflate.findViewById(R.id.search_book_result_list);
+        SearchResultListAdapter adapter = new SearchResultListAdapter(getActivity(), booksFound);
+        searchBookResultList.setAdapter(adapter);
+        searchBookResultList.setOnItemClickListener(bookItemClick());
+
+        return inflate;
+    }
+
+    private AdapterView.OnItemClickListener bookItemClick() {
+        return new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                openBookActivity(booksFound.get(position));
+            }
+        };
+    }
+
+    private void openBookActivity(Book book) {
+        Intent intent = new Intent(getActivity().getApplicationContext(), BookActivity.class);
+        intent.putExtra("book", book);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/pl/edu/utp/mybookshelf/adapter/SearchResultListAdapter.java
@@ -0,0 +1,92 @@
+package pl.edu.utp.mybookshelf.adapter;
+
+import android.app.Activity;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import pl.edu.utp.mybookshelf.R;
+import pl.edu.utp.mybookshelf.model.Book;
+
+public class SearchResultListAdapter implements ListAdapter {
+
+    private final Activity context;
+    private final List<Book> books;
+
+    public SearchResultListAdapter(Activity context, List<Book> books) {
+        this.context = context;
+        this.books = books;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) { }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) { }
+
+    @Override
+    public int getCount() {
+        return books.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return books.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.book_list_item, null,true);
+
+        TextView titleText = rowView.findViewById(R.id.book_title);
+        TextView authorText = rowView.findViewById(R.id.book_author);
+        ImageView imageView = rowView.findViewById(R.id.book_image);
+
+        titleText.setText(books.get(position).getTitle());
+        authorText.setText(books.get(position).getAuthor());
+        imageView.setImageResource(books.get(position).getImage());
+
+        return rowView;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return books.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return true;
+    }
+}
