3406ed8b04868087926d4c617605300dcc36d7e0
mbalcer <mateusz75319@gmail.com>
2021-06-04 15:54:54 +0200

    Implemented searching books

--- app/src/main/java/pl/edu/utp/mybookshelf/activity/fragment/SearchFragment.java
+++ app/src/main/java/pl/edu/utp/mybookshelf/activity/fragment/SearchFragment.java
@@ -2,6 +2,8 @@ package pl.edu.utp.mybookshelf.activity.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +12,13 @@ import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import pl.edu.utp.mybookshelf.R;
 import pl.edu.utp.mybookshelf.activity.BookActivity;
@@ -22,6 +28,7 @@ import pl.edu.utp.mybookshelf.model.Category;
 
 public class SearchFragment extends Fragment {
 
+    private List<Book> allBooks = new ArrayList<>();
     private List<Book> booksFound = new ArrayList<>();
     private ListView searchBookResultList;
 
@@ -36,11 +43,11 @@ public class SearchFragment extends Fragment {
                         "11. urodzin bohater dowiaduje się, że istnieje świat, o którym nie miał pojęcia", R.drawable.book_1,
                 "9877323132", 326, LocalDate.of(1997, 10, 1), new Category(1l, "Fantasy"), null);
 
-        booksFound.add(hp);
-        booksFound.add(new Book("J.K. Rowling", "Harry Potter i Komnata Tajemnic", R.drawable.book_1));
-        booksFound.add(new Book("J.K. Rowling", "Harry Potter i Więzień Azkabanu", R.drawable.book_1));
-        booksFound.add(new Book("J.K. Rowling", "Harry Potter i Czara Ognia", R.drawable.book_1));
-        booksFound.add(new Book("Adam Mickiewicz", "Pan Tadeusz", R.drawable.book_2));
+        allBooks.add(hp);
+        allBooks.add(new Book("J.K. Rowling", "Harry Potter i Komnata Tajemnic", R.drawable.book_1));
+        allBooks.add(new Book("J.K. Rowling", "Harry Potter i Więzień Azkabanu", R.drawable.book_1));
+        allBooks.add(new Book("J.K. Rowling", "Harry Potter i Czara Ognia", R.drawable.book_1));
+        allBooks.add(new Book("Adam Mickiewicz", "Pan Tadeusz", R.drawable.book_2));
     }
 
     @Override
@@ -49,9 +56,40 @@ public class SearchFragment extends Fragment {
         View inflate = inflater.inflate(R.layout.fragment_search, container, false);
 
         searchBookResultList = inflate.findViewById(R.id.search_book_result_list);
-        SearchResultListAdapter adapter = new SearchResultListAdapter(getActivity(), booksFound);
-        searchBookResultList.setAdapter(adapter);
-        searchBookResultList.setOnItemClickListener(bookItemClick());
+
+        TextInputEditText searchBookText = inflate.findViewById(R.id.search_book_text);
+        searchBookText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().length() > 3) {
+                    booksFound = allBooks.stream()
+                            .filter(book -> {
+                                int indexTitle = book.getTitle().toLowerCase().indexOf(String.valueOf(s));
+                                int indexAuthor = book.getAuthor().toLowerCase().indexOf(String.valueOf(s));
+                                int indexIsbn = (book.getIsbn() != null) ? book.getIsbn().indexOf(String.valueOf(s)) : -1;
+
+                                return (indexTitle != -1 || indexAuthor != -1 || indexIsbn != -1) ? true : false;
+                            })
+                            .collect(Collectors.toList());
+
+                    if (booksFound.size() > 0) {
+                        SearchResultListAdapter adapter = new SearchResultListAdapter(getActivity(), booksFound);
+                        searchBookResultList.setAdapter(adapter);
+                        searchBookResultList.setOnItemClickListener(bookItemClick());
+                    } else {
+                        SearchResultListAdapter adapter = new SearchResultListAdapter(getActivity(), Arrays.asList(new Book("Kliknij tutaj aby dodać nową książkę", "Nie znaleziono szukanej książki")));
+                        searchBookResultList.setAdapter(adapter);
+                        searchBookResultList.setOnItemClickListener(null);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
 
         return inflate;
     }
