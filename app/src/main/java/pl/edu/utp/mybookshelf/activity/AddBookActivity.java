93cae6875184bd7cd5fd561a1e66e219148efa34
mbalcer <mateusz75319@gmail.com>
2021-06-11 19:53:40 +0200

    Created add book activity

---
+++ app/src/main/java/pl/edu/utp/mybookshelf/activity/AddBookActivity.java
@@ -0,0 +1,15 @@
+package pl.edu.utp.mybookshelf.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pl.edu.utp.mybookshelf.R;
+
+public class AddBookActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+    }
+}
