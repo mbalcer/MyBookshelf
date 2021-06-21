b599924ae21a9210f349a2c3c6dd9802c4937205
mbalcer <mateusz75319@gmail.com>
2021-06-21 13:31:45 +0200

    Added button to add/delete book to/from bookshelf

--- app/src/main/java/pl/edu/utp/mybookshelf/activity/BookActivity.java
+++ app/src/main/java/pl/edu/utp/mybookshelf/activity/BookActivity.java
@@ -2,6 +2,7 @@ package pl.edu.utp.mybookshelf.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
@@ -24,6 +25,7 @@ import pl.edu.utp.mybookshelf.activity.fragment.BookInfoFragment;
 import pl.edu.utp.mybookshelf.activity.fragment.BookMyDataFragment;
 import pl.edu.utp.mybookshelf.activity.fragment.BookReviewsFragment;
 import pl.edu.utp.mybookshelf.adapter.ViewPagerAdapter;
+import pl.edu.utp.mybookshelf.database.DBHelper;
 import pl.edu.utp.mybookshelf.model.Book;
 import pl.edu.utp.mybookshelf.model.Review;
 
@@ -32,6 +34,7 @@ public class BookActivity extends AppCompatActivity {
     private ActionBar toolbar;
 
     private Book book;
+    private DBHelper dbHelper;
     private List<String> titleTabs = new ArrayList<>();
     private List<Fragment> tabs = new ArrayList<>();
 
@@ -40,6 +43,7 @@ public class BookActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
         toolbar = getSupportActionBar();
+        dbHelper = new DBHelper(getApplicationContext());
         if (getIntent().getExtras() != null && getIntent().getExtras().getSerializable("book") != null) {
             book = (Book) getIntent().getExtras().getSerializable("book");
             toolbar.setTitle(book.getTitle());
@@ -50,7 +54,14 @@ public class BookActivity extends AppCompatActivity {
         TextView titleText = findViewById(R.id.book_title);
         TextView authorText = findViewById(R.id.book_author);
         ImageView imageView = findViewById(R.id.book_image);
+        Button bookInBookshelfBtn = findViewById(R.id.book_in_bookshelf_button);
 
+        if (dbHelper.getUserBookByBookId(book.getId()).getBookId() != null) {
+            bookInBookshelfBtn.setText(R.string.delete_book_from_bookshelf_button);
+            bookInBookshelfBtn.setOnClickListener(bookInBookshelfBtnClickListener(true));
+        } else {
+            bookInBookshelfBtn.setOnClickListener(bookInBookshelfBtnClickListener(false));
+        }
         titleText.setText(book.getTitle());
         authorText.setText(book.getAuthor());
         imageView.setImageResource(book.getImage());
@@ -108,5 +119,19 @@ public class BookActivity extends AppCompatActivity {
 
         titleTabs.add("Opinie");
         tabs.add(new BookReviewsFragment());
+    }
+
+    private View.OnClickListener bookInBookshelfBtnClickListener(boolean delete) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (delete) {
+                    dbHelper.deleteById(book.getId());
+                } else {
+                    dbHelper.setBookAsToRead(book.getId());
+                }
+                recreate();
+            }
+        };
     }
 }
