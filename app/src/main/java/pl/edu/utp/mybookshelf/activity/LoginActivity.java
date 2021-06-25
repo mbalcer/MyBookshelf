cc51f404a6920fe4f76ed13182f7e3186dddd765
mbalcer <mateusz75319@gmail.com>
2021-06-25 21:33:32 +0200

    Added animation switch between login and register and extracted layout login to separate file

--- app/src/main/java/pl/edu/utp/mybookshelf/activity/LoginActivity.java
+++ app/src/main/java/pl/edu/utp/mybookshelf/activity/LoginActivity.java
@@ -1,8 +1,11 @@
 package pl.edu.utp.mybookshelf.activity;
 
 import android.animation.Animator;
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -30,9 +33,25 @@ public class LoginActivity extends AppCompatActivity {
         }
         rootView = findViewById(R.id.loginRootView);
         bookIconImageView = findViewById(R.id.bookIconImageView);
-        loginRegisterView = findViewById(R.id.afterAnimationView);
 
+        initSwitchBetweenLoginAndRegister();
         initCountDownTimer().start();
+    }
+
+    private void initSwitchBetweenLoginAndRegister() {
+        loginRegisterView = findViewById(R.id.loginRegisterLayout);
+        loginRegisterView.findViewById(R.id.loginSignUpButton).setOnClickListener(view -> {
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.layout_register, null);
+
+            loginRegisterView.removeAllViews();
+            loginRegisterView.addView(layout);
+
+            bookIconImageView.animate()
+                    .x(Resources.getSystem().getDisplayMetrics().widthPixels - 278).y(100)
+                    .setDuration(1000)
+                    .setListener(loginRegisterAnimationListener());
+        });
     }
 
     private CountDownTimer initCountDownTimer() {
@@ -49,21 +68,32 @@ public class LoginActivity extends AppCompatActivity {
 
                 rootView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.clouds));
                 bookIconImageView.setImageResource(R.drawable.books);
-                bookIconImageView.animate().x(50).y(100).setDuration(1000).setListener(new Animator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(Animator animator) { }
+                bookIconImageView.animate()
+                        .x(50).y(100)
+                        .setDuration(1000)
+                        .setListener(loginRegisterAnimationListener());
+            }
+        };
+    }
 
-                    @Override
-                    public void onAnimationEnd(Animator animator) {
-                        loginRegisterView.setVisibility(View.VISIBLE);
-                    }
+    private Animator.AnimatorListener loginRegisterAnimationListener() {
+        return new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+                loginRegisterView.setVisibility(View.GONE);
+            }
 
-                    @Override
-                    public void onAnimationCancel(Animator animator) { }
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                loginRegisterView.setVisibility(View.VISIBLE);
+            }
 
-                    @Override
-                    public void onAnimationRepeat(Animator animator) { }
-                });
+            @Override
+            public void onAnimationCancel(Animator animator) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
             }
         };
     }
