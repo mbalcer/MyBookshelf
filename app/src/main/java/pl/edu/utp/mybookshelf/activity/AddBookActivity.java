ad8d91daaf4dffe4c47fbfaf57e2ffa2a64d5031
betlewski <szymonbetlewski@wp.pl>
2021-06-25 17:31:45 +0200

    Created adding new book

--- app/src/main/java/pl/edu/utp/mybookshelf/activity/AddBookActivity.java
+++ app/src/main/java/pl/edu/utp/mybookshelf/activity/AddBookActivity.java
@@ -2,10 +2,12 @@ package pl.edu.utp.mybookshelf.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,20 +24,39 @@ import java.util.Comparator;
 import java.util.List;
 
 import pl.edu.utp.mybookshelf.R;
+import pl.edu.utp.mybookshelf.database.FirebaseBook;
 import pl.edu.utp.mybookshelf.database.FirebaseCallback;
 import pl.edu.utp.mybookshelf.database.FirebaseCategory;
+import pl.edu.utp.mybookshelf.model.Book;
 import pl.edu.utp.mybookshelf.model.Category;
 
 public class AddBookActivity extends AppCompatActivity {
+
+    private TextInputEditText titleEditText;
+    private TextInputEditText authorEditText;
+    private TextInputEditText isbnEditText;
+    private TextInputEditText pagesEditText;
+    private TextInputEditText publishDateEditText;
+    private TextInputEditText imageEditText;
+    private TextInputEditText descriptionEditText;
+
     private MaterialDatePicker<Long> datePicker;
+    private Spinner categorySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
 
+        titleEditText = findViewById(R.id.add_book_title);
+        authorEditText = findViewById(R.id.add_book_author);
+        pagesEditText = findViewById(R.id.add_book_pages);
+        imageEditText = findViewById(R.id.add_book_image);
+        descriptionEditText = findViewById(R.id.add_book_description);
+        categorySpinner = findViewById(R.id.add_book_category);
+
         TextInputLayout isbnLayout = findViewById(R.id.add_book_isbn_layout);
-        TextInputEditText isbnEditText = findViewById(R.id.add_book_isbn);
+        isbnEditText = findViewById(R.id.add_book_isbn);
         isbnLayout.setEndIconOnClickListener(view -> openScanner());
 
         if (getIntent().getExtras() != null && getIntent().getExtras().getSerializable("scannedIsbn") != null) {
@@ -45,7 +66,7 @@ public class AddBookActivity extends AppCompatActivity {
         }
 
         TextInputLayout publishDateLayout = findViewById(R.id.add_book_publish_date_layout);
-        TextInputEditText publishDateEditText = findViewById(R.id.add_book_publish_date);
+        publishDateEditText = findViewById(R.id.add_book_publish_date);
         initDatePicker(publishDateEditText);
 
         publishDateLayout.setEndIconOnClickListener(view -> datePicker.show(getSupportFragmentManager(), AddBookActivity.class.getName()));
@@ -73,7 +94,6 @@ public class AddBookActivity extends AppCompatActivity {
     }
 
     private void initSpinner() {
-        Spinner categorySpinner = findViewById(R.id.add_book_category);
         List<Category> categories = new ArrayList<>();
         ArrayAdapter<Category> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
         FirebaseCategory.getAll(new FirebaseCallback<Category>() {
@@ -86,7 +106,6 @@ public class AddBookActivity extends AppCompatActivity {
         });
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorySpinner.setAdapter(dataAdapter);
-
         categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -98,6 +117,59 @@ public class AddBookActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+        Button addBookButton = findViewById(R.id.add_book_button);
+        addBookButton.setOnClickListener(view -> addBook());
+    }
+
+    private void addBook() {
+        boolean invalidData = false;
+        Editable titleEditable = titleEditText.getText();
+        if (titleEditable == null || titleEditable.toString().trim().isEmpty()) {
+            TextInputLayout titleLayout = findViewById(R.id.add_book_title_layout);
+            titleLayout.setError("To pole jest wymagane");
+            invalidData = true;
+        }
+        Editable authorEditable = authorEditText.getText();
+        if (authorEditable == null || authorEditable.toString().trim().isEmpty()) {
+            TextInputLayout authorLayout = findViewById(R.id.add_book_author_layout);
+            authorLayout.setError("To pole jest wymagane");
+            invalidData = true;
+        }
+        // TODO: checking if isbn unique
+        Editable isbnEditable = isbnEditText.getText();
+        if (isbnEditable == null || isbnEditable.toString().trim().isEmpty()) {
+            TextInputLayout isbnLayout = findViewById(R.id.add_book_isbn_layout);
+            isbnLayout.setError("To pole jest wymagane");
+            invalidData = true;
+        }
+        if (!invalidData) {
+            Book book = new Book();
+            book.setTitle(titleEditable.toString());
+            book.setAuthor(authorEditable.toString());
+            book.setIsbn(isbnEditable.toString());
+
+            Editable pagesEditable = pagesEditText.getText();
+            if (pagesEditable != null && !pagesEditable.toString().trim().isEmpty()) {
+                book.setPages(Integer.valueOf(pagesEditable.toString()));
+            }
+            Editable publishDateEditable = publishDateEditText.getText();
+            if (publishDateEditable != null && !publishDateEditable.toString().trim().isEmpty()) {
+                book.setPublishDate(publishDateEditable.toString());
+            }
+            Editable imageEditable = imageEditText.getText();
+            if (imageEditable != null && !imageEditable.toString().trim().isEmpty()) {
+                book.setImage(imageEditable.toString());
+            }
+            Editable descriptionEditable = descriptionEditText.getText();
+            if (descriptionEditable != null && !descriptionEditable.toString().trim().isEmpty()) {
+                book.setDescription(descriptionEditable.toString());
+            }
+            book.setCategory((Category) categorySpinner.getSelectedItem());
+
+            FirebaseBook.save(book);
+            openBookActivity(book);
+            Log.d(AddBookActivity.class.getName(), "Saved new book: " + titleEditable.toString());
+        }
     }
 
     private void openScanner() {
@@ -105,4 +177,11 @@ public class AddBookActivity extends AppCompatActivity {
         intent.putExtra("addBookActivity", true);
         startActivity(intent);
     }
+
+    private void openBookActivity(Book book) {
+        Intent intent = new Intent(getApplicationContext(), BookActivity.class);
+        intent.putExtra("book", book);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/pl/edu/utp/mybookshelf/database/FirebaseBook.java
+++ app/src/main/java/pl/edu/utp/mybookshelf/database/FirebaseBook.java
@@ -1,6 +1,7 @@
 package pl.edu.utp.mybookshelf.database;
 
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -22,7 +23,7 @@ public class FirebaseBook {
         bookCollection.get().addOnCompleteListener(task -> {
             for (DocumentSnapshot document : task.getResult()) {
                 Book book = new Book();
-                book.setId((Long) document.get("id"));
+                book.setId(document.getId());
                 book.setAuthor((String) document.get("author"));
                 book.setTitle((String) document.get("title"));
                 book.setDescription((String) document.get("description"));
@@ -63,31 +64,10 @@ public class FirebaseBook {
         });
     }
 
-//    public static void initBooks() {
-//        Review r1 = new Review(1L, "Dobra książka", 5, null);
-//        Review r2 = new Review(2L, "Mogłaby być lepsza", 3, null);
-//        Review r3 = new Review(3L, "Nie zachwyca", 2, null);
-//
-//        Book hp = new Book(1L, "J.K. Rowling", "Harry Potter i Kamień Filozoficzny",
-//                "Książka „Harry Potter i Kamień Filozoficzny” rozpoczyna cykl o młodym czarodzieju i jego licznych przygodach. " +
-//                        "Tytułowy Harry Potter wychowywany jest przez nieprzychylnych mu ciotkę i wuja. Jego rodzice zginęli w tajemniczych " +
-//                        "okolicznościach, a jedyne, co mu po nich pozostało to blizna na czole w kształcie błyskawicy. W dniu swoich " +
-//                        "11. urodzin bohater dowiaduje się, że istnieje świat, o którym nie miał pojęcia", R.drawable.book_1,
-//                "9877323132", 326, LocalDate.of(1997, 10, 1).toString(), new Category(1L, "Fantasy"), Arrays.asList(r1, r2, r3));
-//        Book book2 = new Book("J.K. Rowling", "Harry Potter i Komnata Tajemnic", R.drawable.book_1);
-//        book2.setId(2L);
-//        Book book3 = new Book("J.K. Rowling", "Harry Potter i Więzień Azkabanu", R.drawable.book_1);
-//        book3.setId(3L);
-//        Book book4 = new Book("J.K. Rowling", "Harry Potter i Czara Ognia", R.drawable.book_1);
-//        book4.setId(4L);
-//        Book book5 = new Book("Adam Mickiewicz", "Pan Tadeusz", R.drawable.book_2);
-//        book5.setId(5L);
-//
-//        bookCollection.document("1").set(hp);
-//        bookCollection.document("2").set(book2);
-//        bookCollection.document("3").set(book3);
-//        bookCollection.document("4").set(book4);
-//        bookCollection.document("5").set(book5);
-//    }
+    public static void save(Book book) {
+        DocumentReference bookRef = bookCollection.document();
+        bookRef.set(book);
+        book.setId(bookRef.getId());
+    }
 
 }
